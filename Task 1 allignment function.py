ae64546304b930d7c4d7155bce46d65dbe2616a8
CL221057-SandeepSingh <145000268+CL221057-SandeepSingh@users.noreply.github.com>
2024-10-07 12:52:56 +0530

    Update Task 1 allignment function.py

--- Task 1 allignment function.py
+++ Task 1 allignment function.py
@@ -12,13 +12,3 @@ Output : [0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]
 """
 
 #CODE
-def all_alignments(l = int(),m=int()):
-    List = list()
-    for i in range(l+1):
-        for j in range(m+1):
-            a = [i,j]
-            List.append(a)
-    return List
-
-data = all_alignments(2,2)
-print(data)
